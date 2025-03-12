d734e4112c92ae3233a3ad964c81f6457d0704eb
juunghyun <forcomfe81@gmail.com>
2025-03-12 20:04:32 +0900

    GameRestart, GameOver구현
    GameRestart
    -> GameRestartButton을 Start버튼을 복사하여 Onclick메소드만 바꿔서 재사용
    GameOver
    -> GameController에서 시간 갱신하던걸 기반으로 시간이 0이되면 게임 종료 -> Restart버튼과 함께 총 점수 출력하도록 만듬
    
    게임 구현 완료

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,10 +1,17 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using TMPro;
 public class GameController : MonoBehaviour
 {
     [SerializeField] private GameObject panelMainMenu;
     [SerializeField] private GameObject panelInGame;
+    [SerializeField] private GameObject panelGameOver;
     [SerializeField] private TextMeshProUGUI textInGameScore;
+    [SerializeField] private TextMeshProUGUI textGameOverScore;
+    [SerializeField] private Image timeGauge;
+    [SerializeField] private float maxTime = 120f;
 
     private int currentScore = 0;
 
@@ -23,11 +30,42 @@ public class GameController : MonoBehaviour
         panelMainMenu.SetActive(false);
         panelInGame.SetActive(true);
         audioSource.Play();
+        
+        StartCoroutine(nameof(TimeCounter));
     }
 
     public void IncreaseScore(int addScore)
     {
         currentScore += addScore;
         textInGameScore.text = currentScore.ToString();
+    }
+
+    public void ButtonRestart()
+    {
+        SceneManager.LoadScene(0);
+        // SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void GameOver()
+    {
+        textGameOverScore.text = $"SCORE\n{currentScore}";
+
+        panelInGame.SetActive(false);
+        panelGameOver.SetActive(true);
+        audioSource.Stop();
+    }
+
+    private IEnumerator TimeCounter()
+    {
+        float currentTime = maxTime;
+
+        while(currentTime > 0)
+        {
+            currentTime -= Time.deltaTime;
+            timeGauge.fillAmount = currentTime / maxTime;
+
+            yield return null;
+        }
+        GameOver();
     }
 }
